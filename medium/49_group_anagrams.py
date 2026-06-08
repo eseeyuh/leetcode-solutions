d5400d8d809337ff90942cb2588d91c439ad7c71
eseeyuh <shaidarov.daryn@gmail.com>
2026-06-08 20:48:21 +0100

    Add 49. Group Anagrams - Medium

--- medium/49_group_anagrams.py
+++ medium/49_group_anagrams.py
@@ -24,7 +24,9 @@ from typing import List
 
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+
         anagram_map = defaultdict(list)
+        
         for word in strs:
             key = "".join(sorted(word))
             anagram_map[key].append(word)
